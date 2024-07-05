244d610e91edf67f1fcacd9bd70f0fdabb51babc
70hnm4c13l <johnatas@rocketmail.com>
2024-07-05 08:07:26 -0300

    urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from django.http import HttpResponse
-from recipes.views import _test, _home, _contacts
+from recipes.views import _home
 
 
 urlpatterns = [
-    path('sobre/', _test),
     path('', _home),
-    path('contacts/', _contacts),
-
-
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def _test(request):
-    return HttpResponse("About informations....")
 
 def _home(request):
     return render(request, 'recipes/home.html')
-
-def _contacts(request):
-    return HttpResponse('Our contacts here...')
